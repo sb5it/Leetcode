fe8ed48a312adaf3c0dff3381e5187fe6ee441ac
sb5it <155718213+sb5it@users.noreply.github.com>
2024-01-07 11:44:21 +0000

    Solution for 394. Decode String

---
+++ 394/Decode-string-stack.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public string DecodeString(string s) {
+        var stack = new Stack<string>();
+        var repeat = 0;
+        var currentString = new StringBuilder();
+        foreach(var c in s){
+            if(c == '['){
+                stack.Push(currentString.ToString());
+                stack.Push(repeat.ToString());
+                currentString = currentString.Clear();
+                repeat = 0;
+            }  else if(c == ']'){
+                var num = Convert.ToInt32(stack.Pop());
+                var prevString = stack.Pop();
+                var temp = string.Empty;
+                for(int i = 1; i <= num; i++){
+                    temp += currentString.ToString();
+                }
+                currentString.Clear();
+                currentString.Append(prevString);
+                currentString.Append(temp);
+            }
+            else if(char.IsDigit(c)){
+                repeat = repeat*10 + (int)(c -'0');
+            } 
+            else {
+                currentString.Append(c);
+            }
+        }
+        return currentString.ToString();
+    }
+}
