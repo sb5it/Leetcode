cf417e0a1f7f4220b502be344a0ffc5e90157928
sb5it <155718213+sb5it@users.noreply.github.com>
2024-01-07 11:58:39 +0000

    Using string builder for temp

--- 394/Decode-string-stack.cs
+++ 394/Decode-string-stack.cs
@@ -12,13 +12,13 @@ public class Solution {
             }  else if(c == ']'){
                 var num = Convert.ToInt32(stack.Pop());
                 var prevString = stack.Pop();
-                var temp = string.Empty;
+                var temp = new StringBuilder();
                 for(int i = 1; i <= num; i++){
-                    temp += currentString.ToString();
+                    temp.Append(currentString.ToString());
                 }
                 currentString.Clear();
                 currentString.Append(prevString);
-                currentString.Append(temp);
+                currentString.Append(temp.ToString());
             }
             else if(char.IsDigit(c)){
                 repeat = repeat*10 + (int)(c -'0');
