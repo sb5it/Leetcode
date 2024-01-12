bce200ae3282367dfde15b0de3679670f3eda09e
sb5it <155718213+sb5it@users.noreply.github.com>
2024-01-12 07:18:49 +0000

    Solution without if statement

--- 121/Best-time-to-buy-and-sell-stock.cs
+++ 121/Best-time-to-buy-and-sell-stock.cs
@@ -1,14 +1,11 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int left = 0, profit = 0, maxProfit = 0;
+        int minPrice = int.MaxValue, profit = 0, maxProfit = 0;
         
-        for (int right = 1; right < prices.Length; right++) {
-             profit = prices[right]-prices[left];
-            if (profit <= 0) {
-                left = right;
-            } else {
-                maxProfit = Math.Max(maxProfit, profit);
-            }
+        for(int i = 0; i < prices.Length; i++) {
+            minPrice = Math.Min(minPrice, prices[i]);
+            profit = prices[i] - minPrice;
+            maxProfit = Math.Max(maxProfit, profit);
         }
         return maxProfit;
     }
