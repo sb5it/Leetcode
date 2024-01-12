7eb87c0c0ba3685ef21b60decd6a92e603e1ff47
sb5it <155718213+sb5it@users.noreply.github.com>
2024-01-12 07:13:05 +0000

    Using for loop

--- 121/Best-time-to-buy-and-sell-stock.cs
+++ 121/Best-time-to-buy-and-sell-stock.cs
@@ -1,15 +1,14 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int left = 0, right = 1, profit = 0, maxProfit = 0;
+        int left = 0, profit = 0, maxProfit = 0;
         
-        while (right < prices.Length){
-            profit = prices[right]-prices[left];
-            if (profit <= 0){
+        for (int right = 1; right < prices.Length; right++) {
+             profit = prices[right]-prices[left];
+            if (profit <= 0) {
                 left = right;
             } else {
                 maxProfit = Math.Max(maxProfit, profit);
             }
-            right++;
         }
         return maxProfit;
     }
