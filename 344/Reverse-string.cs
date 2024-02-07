861fa1a6715b021a6a6969a949ef43e6675e6245
sb5it <155718213+sb5it@users.noreply.github.com>
2024-02-07 07:50:27 +0000

    Solution for Reverse String

---
+++ 344/Reverse-string.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        int left = 0, right = s.Length - 1;
+        char temp = 'c';
+        while(left < right) {
+            temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
