24f80e0444645b94d9bd038450494ca739b90d58
sb5it <155718213+sb5it@users.noreply.github.com>
2024-10-17 07:00:08 +0100

    Create Sum-root-to-leaf-numbers.cs

---
+++ 129/Sum-root-to-leaf-numbers.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    private int sum = 0;
+    public int SumNumbers(TreeNode root) {
+        Traverse(root, 0);
+        return sum;
+    }
+
+    private void Traverse(TreeNode node, int currentSum){
+        if(node == null) return;
+        currentSum *= 10;
+        currentSum+= node.val; 
+
+        if(node.right is null && node.left is null){
+            sum += currentSum;
+        }
+        if (node.left is not null) Traverse(node.left, currentSum);
+        if (node.right is not null) Traverse(node.right, currentSum);
+
+        currentSum-=node.val;
+        currentSum /= 10;
+    }
+}
